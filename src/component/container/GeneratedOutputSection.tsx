3e4a121588d1af4b2d1c178917a5db956230b257
chamarabandarawd <chamarabandarawd@gmail.com>
2023-09-10 00:08:41 +0530

    icons were added for user input and output

--- src/component/container/GeneratedOutputSection.tsx
+++ src/component/container/GeneratedOutputSection.tsx
@@ -1,14 +1,23 @@
 import { Avatar } from '@mui/material';
 import { deepPurple } from '@mui/material/colors';
+import ContentCopyOutlinedIcon from '@mui/icons-material/ContentCopyOutlined';
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 import React from 'react'
 
 function GeneratedOutputSection() {
   return (
     <div className='user_input_section'>
     <Avatar sx={{ bgcolor: deepPurple[500] }}>PS</Avatar>
-    <div className='req_para'>MessageM essageMessage Messag eMessageMessageM essageMessage Messag eMessage
+    <div className='req_para'>MessageMeM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag eMesseM essageMessage Messag 
+    eMesseM essageMessage Messag eMesseM essageMessage Messag eMess essageMessage Messag eMessageMessageM essageMessage Messag eMessage
     MessageM essageMessage Messag eMessage
     MessageM essageMessage Messag eMessage</div>
+    <div className='icon'>
+    <ContentCopyOutlinedIcon/>
+    <ThumbUpAltOutlinedIcon/>
+    <ThumbDownAltOutlinedIcon/>
+    </div>
     </div>
   )
 }
--- src/component/container/UserInputSection.tsx
+++ src/component/container/UserInputSection.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Avatar from '@mui/material/Avatar';
 import { deepPurple } from '@mui/material/colors';
+import EditNoteOutlinedIcon from '@mui/icons-material/EditNoteOutlined';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 
 function UserInputSection() {
   return (
@@ -8,7 +10,14 @@ function UserInputSection() {
     <Avatar sx={{ bgcolor: deepPurple[500] }}>CB</Avatar>
     <div className='req_para'>MessageM essageMessage Messag eMessageMessageM essageMessage Messag eMessage
     MessageM essageMessage Messag eMessage
-    MessageM essageMessage Messag eMessage</div>
+    MessageM essageMessage Messag eMessage
+    eM essageMessage Messag eMess
+    eM essageMessage Messag eMess</div>
+    <div className='icon'>
+    <EditNoteOutlinedIcon/>
+    <SaveOutlinedIcon/>
+    </div>
+
     </div>
   )
 }
