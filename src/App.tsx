126b2744b4651b53d8d85a0318f009e089c74c34
chamarabandarawd <chamarabandarawd@gmail.com>
2023-10-01 11:45:26 +0530

    done correction to deloyement

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,4 @@
-import React from 'react';
 import "./App.css";
-import Container from './component/container/Container';
-import SideBar from './component/sideBar/SideBar';
-import LandingPage from './component/LandingPage';
-import InsideApp from './InsideApp';
 import RouteComponent from './RouteComponent';
 
 
